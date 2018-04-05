ac85c7819b75abb88e1ffb9762d35b7c03816f87
MrJoyner95 <oct_romo@hotmail.com>
2018-04-05 02:23:39 -0500

    Comprobaciones para mostrar datos en el Grid agregadas

--- BlitzWolf/CustomControlInicio.cs
+++ BlitzWolf/CustomControlInicio.cs
@@ -52,23 +52,32 @@ namespace BlitzWolf
             dataGridView_Instancias.Columns.Clear();
             dataGridView_Instancias.Rows.Clear();
 
-            // Agrega columnas al Grid:
-            foreach(Global.Attribute atributo in Global.DataSet_Attributes)
+            // Comprueba que hayan valores para los datos del DataSet:
+            if (Global.DataSet_Attributes.Count > 0 && Global.DataSet_Data.Count > 0)
             {
-                // Parametros: (name, header)
-                dataGridView_Instancias.Columns.Add(atributo.name, atributo.name);
-            }
+                // Agrega columnas al Grid:
+                foreach (Global.Attribute atributo in Global.DataSet_Attributes)
+                {
+                    // Parametros: (name, header)
+                    dataGridView_Instancias.Columns.Add(atributo.name, atributo.name);
+                }
 
-            // Agrega instancias al Grid:
-            foreach(string[] instancia in Global.DataSet_Data)
-            {
-                dataGridView_Instancias.Rows.Add(instancia);
-            }
+                // Agrega instancias al Grid:
+                foreach (string[] instancia in Global.DataSet_Data)
+                {
+                    dataGridView_Instancias.Rows.Add(instancia);
+                }
 
-            // Marca atributos que no cumplen con su expresion regular:
-            foreach (int[] posicionAtributo in Global.listaAtributosErroneos)
+                // Marca atributos que no cumplen con su expresion regular:
+                foreach (int[] posicionAtributo in Global.listaAtributosErroneos)
+                {
+                    dataGridView_Instancias.Rows[posicionAtributo[0]].Cells[posicionAtributo[1]].Style.BackColor = Color.Red;
+                }
+            }
+            else
             {
-                dataGridView_Instancias.Rows[posicionAtributo[0]].Cells[posicionAtributo[1]].Style.BackColor = Color.Red;
+                // El DataSet no cuenta con los valores necesarios para ser mostrado:
+                MessageBox.Show("El conjunto de datos no cuenta con los valores necesarios para ser mostrado en el Grid.", "Error: No puede mostrarse el conjunto de datos.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
 
